f430f9b8f40dcbdd07731ad99b5060990db761c6
IrfanSabbir <avoidirfan@gmail.com>
2023-08-28 21:59:12 +0600

    Added new auth route's and modified user model

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router();
 
@@ -17,13 +19,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters')
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -34,6 +31,18 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign(
+      {
+        id: user._id,
+        email: user.email
+      },
+      process.env.JWT_KEY!
+    )
+    
+    req.session = {
+      jwt: userJwt
+    }
 
     res.status(201).send(user);
   }
