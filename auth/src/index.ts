a02475f0d53e67c2da241c3d0b6eb7084be519e8
IrfanSabbir <avoidirfan@gmail.com>
2023-08-28 21:53:58 +0600

    cookie configaretion added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import "express-async-errors"
 import mongoose from "mongoose"
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,8 +12,14 @@ import { NotFoundError } from './errors/not-found-error';
 
 const PORT = 3000;
 const app: Express = express();
+app.set('trust proxy', true);
 app.use(express.json());
-
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,16 +31,18 @@ app.get("/api/users/test", (req: Request, res: Response) => {
   })
 });
 
-
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
-
 
 app.use(errorHandler);
 
 const start = async () => {
   try {
+
+    if(!process.env.JWT_KEY) {
+      throw new Error('JWT_KEY must be defined');
+    }
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
 
     console.log('Connected to MongoDb');
