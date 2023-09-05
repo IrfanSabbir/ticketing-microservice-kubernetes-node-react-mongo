16d11d7675d2425c1a011abc8f2d3ae8c95f13a9
IrfanSabbir <avoidirfan@gmail.com>
2023-09-05 20:22:25 +0600

    auth restructured

---
+++ auth/src/app.ts
@@ -0,0 +1,39 @@
+import express, { Express, Request, Response } from "express";
+import "express-async-errors"
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+
+const app: Express = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.get("/api/users/test", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Test api call for auth success"
+  })
+});
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,41 +1,8 @@
-import express, { Express, Request, Response } from "express";
-import "express-async-errors"
 import mongoose from "mongoose"
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { app } from './app';
 
 const PORT = 3000;
-const app: Express = express();
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-app.get("/api/users/test", (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "Test api call for auth success"
-  })
-});
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 const start = async () => {
   try {
