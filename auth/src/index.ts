bbbecd43835269baf8887507b841ee8d6afc6c20
IrfanSabbir <avoidirfan@gmail.com>
2024-04-25 20:57:03 +0600

    added jwt key in auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,13 +5,15 @@ import { app } from './app';
 const PORT = 3000;
 
 const start = async () => {
+  if(!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
   try {
-
-    if(!process.env.JWT_KEY) {
-      throw new Error('JWT_KEY must be defined');
-    }
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
