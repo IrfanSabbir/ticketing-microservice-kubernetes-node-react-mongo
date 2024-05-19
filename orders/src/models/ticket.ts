08ed7ca415dc7a19583438afa931dab9ee8b69a3
IrfanSabbir <avoidirfan@gmail.com>
2024-05-19 13:21:52 +0600

    Created Order andTicket MOdal for Order service

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import { Order, OrderStatus } from "./order";
+
+interface TicketAtter {
+  id: string;
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document{
+  title: string;
+  price: number;
+  isReserved(): Promise<boolean>;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAtter) : TicketDoc;
+}
+
+const ticketSchema =  new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id =  ret._id;
+      delete ret._id;
+    }
+  }
+})
+
+ticketSchema.statics.build = (attrs: TicketAtter) => {
+  return new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price
+  });
+}
+
+ticketSchema.methods.isReserved = async function() {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Complete,
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+      ]
+    }
+  })
+  return !!existingOrder
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket }
