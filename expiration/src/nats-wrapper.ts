6c946726ea1a8fb7bf3100c18e47bff4dd2bf4da
IrfanSabbir <avoidirfan@gmail.com>
2024-05-21 12:38:01 +0600

    expiration service created with simple folder structure and docker image

---
+++ expiration/src/nats-wrapper.ts
@@ -0,0 +1,28 @@
+import nats, {Stan} from 'node-nats-streaming'
+class NatsWrapper {
+  private _client?: Stan
+
+  get client(){
+    if(!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    }
+    return this._client;
+  }
+
+  connect(clusterID: string, clientID: string, url: string) {
+    this._client = nats.connect(clusterID, clientID, { url });
+
+    return new Promise<void> ((resolve, reject) => {
+      this._client?.on('connect', () => {
+        console.log('Connected to NATS')
+        resolve()
+      })
+      this._client?.on('error', (err) => {
+        reject(err)
+      })
+    })
+
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
